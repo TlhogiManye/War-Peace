e927417de8b6b5227f92ee90b3480432dc7b8091
Tlhogi Manye <tlhogi.manye@byteorbit.com>
2024-07-22 17:21:19 +0200

    Generate WordFrequency class to track freqency of each word

---
+++ app/src/main/java/com/example/war_peace/models/WordFrequency.java
@@ -0,0 +1,19 @@
+package com.example.war_peace.models;
+
+public class WordFrequency {
+    private String word;
+    private int frequency;
+
+    public WordFrequency(String word, int frequency){
+        this.word = word;
+        this.frequency = frequency;
+    }
+    public String getWord() {
+        return word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+}
